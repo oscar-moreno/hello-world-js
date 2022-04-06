b2438664703aa980c3fbc30ed71fd41f553ed273
Oscar Moreno <omoreno@excentia.es>
2022-04-06 17:15:12 +0200

    console.log removed

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Hello world!");
+let number = 1;
+let number2 = 0;
+let sum = number + number2;
+console.log(sum);
+let result = number / number2;
+let result3 = number + 100;
+let result4 = number2 + 100;
+let result5 = sum + 100;
+let result5 = result + 100;
+
+const greet = "Hello world!";
+
+console.log(greet);
+console.log("Hello world!");
+
